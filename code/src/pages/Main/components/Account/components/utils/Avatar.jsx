827035d16c9388078f66af7a302923857383be18
Akbar <akbarjonjorayev001@gmail.com>
2024-03-31 15:37:57 +0500

    new -> Avatar

---
+++ code/src/pages/Main/components/Account/components/utils/Avatar.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+
+import { load } from '../../../../../../js/db/db'
+import { loadLocal } from '../../../../../../js/db/localStorage'
+
+import './Avatar.css'
+
+async function getName() {
+  const username = loadLocal('quran').accounts.active
+  const name = await load(`accounts/${username}/name`)
+
+  return name[0]
+}
+
+function Avatar({ style }) {
+  const [name, setName] = useState(() => '')
+
+  useEffect(() => {
+    if (name) setName(name)
+
+    async function loadImg() {
+      setName(await getName())
+      console.log('get from db')
+    }
+    loadImg()
+  }, [])
+
+  return (
+    <div className="avatar df_f_ce con_bd_cl" style={style}>
+      <span>{name}</span>
+    </div>
+  )
+}
+
+export default Avatar
