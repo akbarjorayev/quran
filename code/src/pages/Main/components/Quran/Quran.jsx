0d4d637508969934a0a12f38c97b555bf040e9c9
Akbar <akbarjonjorayev001@gmail.com>
2024-04-04 17:16:33 +0500

    Quran Page introduction

--- code/src/pages/Main/components/Quran/Quran.jsx
+++ code/src/pages/Main/components/Quran/Quran.jsx
@@ -1,7 +1,20 @@
+import LastRead from './components/lastRead/LastRead'
+import Choose from '../../../../components/Choose/Choose'
+
 import './Quran.css'
 
 function Quran() {
-  return <h1>Quran</h1>
+  return (
+    <>
+      <div className="list_y">
+        <LastRead surah="Al-Fatiha" verse="1" iOption="surahs" />
+        <Choose axe="x">
+          <div option="surahs">Surahs</div>
+          <div option="juzs">Juz</div>
+        </Choose>
+      </div>
+    </>
+  )
 }
 
 export default Quran
